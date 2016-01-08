ccc387eb04f55fde08202a68232d24058ed61e78
Thomas Boerger <tboerger@suse.de>
2016-01-08 15:43:20 +0100

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: steam
+# Attributes:: default
+#
+# Copyright 2013-2014, Thomas Boerger <thomas@webhippie.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default["steam"]["packages"] = %w(
+  steam
+)
+
+case node["platform_family"]
+when "suse"
+  repo = case node["platform_version"]
+  when /\A13\.\d+\z/
+    "openSUSE_#{node["platform_version"]}"
+  when /\A42\.\d+\z/
+    "openSUSE_Leap_#{node["platform_version"]}"
+  when /\A\d{8}\z/
+    "openSUSE_Tumbleweed"
+  else
+    raise "Unsupported SUSE version"
+  end
+
+  default["steam"]["zypper"]["enabled"] = true
+  default["steam"]["zypper"]["alias"] = "games-tools"
+  default["steam"]["zypper"]["title"] = "Games Tools"
+  default["steam"]["zypper"]["repo"] = "http://download.opensuse.org/repositories/games:/tools/#{repo}/"
+  default["steam"]["zypper"]["key"] = "#{node["steam"]["zypper"]["repo"]}repodata/repomd.xml.key"
+end
---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name "steam"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures steam"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+depends "zypper"
+recipe "steam", "Installs/Configures steam"
+
+supports "suse", ">= 12.3"
+supports "debian", ">= 7.0"
+supports "ubuntu", ">= 12.04"
